5d67a9d7d51b2cad4450c7e776361cef4c493882
Denis Gimaldinov <d.gimaldinov@spb.tpgtaipit.ru>
2025-07-11 09:26:33 +0300

    FIX jsp and servlets

--- src/main/java/com/tictactoe/LogicServlet.java
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -7,25 +7,70 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 @WebServlet(name = "LogicServlet", urlPatterns = "/logic")
 public class LogicServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
-        Object field = session.getAttribute("field");
-
-        if (Field.class != field.getClass()) {
-
-        }
+        Field field = extractField(session);
 
         int index = getSelectedIndex(req);
-        resp.sendRedirect("index.jsp");
+
+        Map<Integer, Sign> fieldData = field.getField();
+        if (fieldData.get(index) != Sign.EMPTY) {
+            getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
+            return;
+        }
+
+        fieldData.put(index, Sign.CROSS);
+
+        if (checkWin(resp, session, field)) return;
+
+        int indexFieldEmpty = field.getEmptyFieldIndex();
+
+        if (indexFieldEmpty >= 0) {
+            fieldData.put(indexFieldEmpty, Sign.NOUGHT);
+            if (checkWin(resp, session, field)) return;
+        }
+
+        List<Sign> data = field.getFieldData();
+
+        session.setAttribute("field", field);
+        session.setAttribute("data", data);
+
+        resp.sendRedirect("/index.jsp");
+
     }
 
     private static int getSelectedIndex(HttpServletRequest req) {
         String click = req.getParameter("click");
         boolean isNumeric = click.chars().allMatch(Character::isDigit);
-        return isNumeric ? Integer.parseInt(click): 0;
+        return isNumeric ? Integer.parseInt(click) : 0;
+    }
+
+    private static Field extractField(HttpSession session) {
+        Object field = session.getAttribute("field");
+
+        if (Field.class != field.getClass()) {
+            session.invalidate();
+            throw new RuntimeException("Session is broken, try one more time");
+        }
+        return (Field) field;
+    }
+
+    private boolean checkWin(HttpServletResponse resp, HttpSession session, Field field) throws IOException {
+        Sign winner = field.checkWin();
+        if (winner == Sign.CROSS || winner == Sign.NOUGHT) {
+            session.setAttribute("winner", winner);
+
+            List<Sign> data = field.getFieldData();
+            session.setAttribute("data", data);
+            resp.sendRedirect("/index.jsp");
+            return true;
+        }
+        return false;
     }
 }
